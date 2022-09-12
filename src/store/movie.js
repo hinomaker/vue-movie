4f48b4e58d3d80732bba6ffa9960e16fe6c983ab
hinomaker <chh5946@gmail.com>
2022-09-12 09:31:28 +0900

    Add Netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -50,23 +50,28 @@ export default{
            for(let page = 2; page <= pageLength; page+= 1){
               if(page > (payload.number / 10)) break
           
-              const res = await _fetchMovie({
-              ...payload,
-              page
+               const res = await _fetchMovie({
+               ...payload,
+               page
               })
               const {Search} = res.data
+              console.log(Search)
               commit('updateState', {
                movies: [
                 ...state.movies, 
                 ..._.uniqBy(Search, 'imdbID')
                ]
               })
-           }
+            }
           }
-        }catch(message){
+        }catch({message}){
          commit('updateState', {
             movies: [],
             message
+          })
+        }finally{
+          commit('updateState',{
+            loading : false
           })
         }
     },
@@ -95,23 +100,6 @@ export default{
   }
 }
 
-function _fetchMovie(payload){
-  const {title, type, year, page, id} = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-      .then(res => {
-        if(res.data.Error){
-          reject(res.data.Error)
-        }
-        resolve(res)
-      })
-      .catch(err => {
-        reject(err.message)
-      })
-  })
+async function _fetchMovie(payload){
+  return await axios.post('/.netlify/functions/movie', payload)
 }
